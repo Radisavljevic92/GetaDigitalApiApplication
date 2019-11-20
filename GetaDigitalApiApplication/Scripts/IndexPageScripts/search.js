66665dfcb03b84fbc6b4b92733ac04e23ef2ab8a
Ilija Radisavljević <ilija2603@gmail.com>
2019-11-20 10:51:43 +0100

    First commit

---
+++ GetaDigitalApiApplication/Scripts/IndexPageScripts/search.js
@@ -0,0 +1,43 @@
+﻿// Your use of the YouTube API must comply with the Terms of Service:
+// https://developers.google.com/youtube/terms
+// Called automatically when JavaScript client library is loaded.
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+// Called automatically when YouTube API interface is loaded.
+function onYouTubeApiLoad() {
+//  gapi.client.setapikey('replace_with_your_youtube_api_key');
+    gapi.client.setApiKey('AIzaSyDaA75gdTiOXrbdEpMKwHboZtijz4_Ah54');
+}
+function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
+$(function () {
+    $("form").on("submit", function (e) {
+        e.preventDefault();
+        // prepare the request
+        var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 5,
+            order: "viewCount",
+            publishedAfter: "2018-01-01T00:00:00Z"
+        });
+        // execute the request
+        request.execute(function (response) {
+            var results = response.result;
+            $("#results").html("");
+            $.each(results.items, function (index, item) {
+                $.get("tpl/Item.html", function (data) {
+                    $("#results").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
+                });
+            });
+            resetVideoHeight();
+        });
+    });
+
+    $(window).on("resize", resetVideoHeight);
+});
+
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9 / 16);
+}
